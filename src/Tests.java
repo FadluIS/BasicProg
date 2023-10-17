b8b476900258f02a847fec424d6b5605754a88d7
Fadlurahman <fadlelrahman.abdelrehim@scout24.com>
2023-10-18 01:01:29 +0200

    Chapter 5 - encode

--- src/Tests.java
+++ src/Tests.java
@@ -2,7 +2,8 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Tests {
     public static void main(String[] args) {
-        System.out.println(delimitedString(args[0], args[1].charAt(0), args[2].toCharArray()[0]));
+//        System.out.println(delimitedString(args[0], args[1].charAt(0), args[2].toCharArray()[0]));
+//        System.out.println(encode("äzXyä"));
     }
 
     /**
@@ -14,7 +15,7 @@ public class Tests {
      * @param end Ending character
      * @return substring of s
      */
-    public static String delimitedString( String s, char start, char end) {
+    public static String delimitedString(String s, char start, char end) {
         // Iterate over string until start is found
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) != start) continue;
@@ -32,5 +33,34 @@ public class Tests {
 
         // Return null if substring not found
         return null;
+    }
+
+    /**
+     * Takes {@code text} into lowercase and replaces each letter with its
+     * numerical position in the alphabet.
+     *
+     * Umlauts are converted to their English counterparts.
+     *
+     * @param text
+     * @return {@code String} encoded text
+     */
+    public static String encode(String text) {
+        // Take text into lowercase
+        text = text.toLowerCase();
+
+        // Replace all umlauts
+        text = text.replace("ä", "ae");
+        text = text.replace("ß", "ss");
+        text = text.replace("ü", "ue");
+        text = text.replace("ö", "oe");
+
+        // Iterate over each letter replacing it with equivalent numerical
+        // position in the alphabet
+        String result = "";
+        for (int i = 0; i < text.length(); i++) {
+            result += text.charAt(i) - 'a' + 1;
+        }
+
+        return result;
     }
 }
