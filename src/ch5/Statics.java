1b3ab090d06fb9fc81b24d3437c910e315d0c9e8
Fadlurahman <fadlelrahman.abdelrehim@scout24.com>
2023-10-19 13:21:45 +0200

    Chapter 5 - extract

--- src/ch5/Statics.java
+++ src/ch5/Statics.java
@@ -1,11 +1,34 @@
 package ch5;
 
+import java.util.Arrays;
 import java.util.Vector;
 
 public class Statics {
     public static void main(String[] args) {
 //        System.out.println(delimitedString(args[0], args[1].charAt(0), args[2].toCharArray()[0]));
 //        System.out.println(encode("äzXyä"));
+
+        // Test encode
+        String delimiter = "|";
+        String original = "Hi! We'll be <br> using| pipes| as <br> our| delimiter.";
+        String[] expected = {"Hi! We'll be <br> using", " pipes", " as <br> our", " delimiter."};
+        String[] result = extract(original, delimiter);
+
+        System.out.format("Original string: %s%n", original);
+        System.out.format("Expected string: %s%n", Arrays.toString(expected));
+        System.out.format("Result string: %s%n", Arrays.toString(result));
+
+        System.out.println("\n\n--------------\n\n");
+
+        // Test encode long
+        String delimiterLong = "<br>";
+        String originalLong = "Hi! We'll be<br><br> <br> using| breaks| as <br> our| delimiter.<br><br>";
+        String[] expectedLong = {"Hi! We'll be", " ", " using| breaks| as ", " our| delimiter."};
+        String[] resultLong = extract(originalLong, delimiterLong);
+
+        System.out.format("Original string: %s%n", originalLong);
+        System.out.format("Expected string: %s%n", Arrays.toString(expectedLong));
+        System.out.format("Result string: %s%n", Arrays.toString(resultLong));
     }
 
     /**
@@ -67,14 +90,42 @@ public class Statics {
     }
 
     public static String[] extract(String text, String delim) {
-        Vector<String> buffer = new Vector<>();
+        Vector<String> buffer = new Vector<String>();
         String word = "";
 
         // Using two pointers to navigate words and delimiters, iterate over entire string
         for (int i = 0; i < text.length(); i++) {
+            char curr = text.charAt(i);
+            if (curr != delim.charAt(0)) {
+                word += curr;
+                continue;
+            }
 
+            // Check when delimiter is found
+            boolean found = true;
+            String temp = String.valueOf(curr);
+            for (int j = 1; j < delim.length(); j++, i++) {
+                if (text.charAt(i + 1) != delim.charAt(j)) {
+                    found = false;
+                    break;
+                }
+
+                temp += delim.charAt(j);
+            }
+
+            if (found) {
+                if(word.length() > 0) buffer.add(word);
+                word = "";
+            } else {
+                word += temp;
+            }
         }
 
-        return null;
+        if (word.length() > 0) {
+            buffer.add(word);
+        }
+
+        String[] a = {};
+        return buffer.toArray(a);
     }
 }
